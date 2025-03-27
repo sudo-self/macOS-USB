178b7fa65d7dedd5f497adf69ff52eaa7a4190a6
System Administrator <root@Jesse.local>
2025-03-27 15:19:03 -0600

    android

---
+++ app/src/main/java/xyz/jessejesse/usb/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package xyz.jessejesse.usb.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
